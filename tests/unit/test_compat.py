c9817cfcf7a67bd5e8fde89546038b7198c3c0b3
Greg Lindahl <lindahl@pbm.com>
2018-08-28 21:04:57 -0700

    more tests

---
+++ tests/unit/test_compat.py
@@ -0,0 +1,27 @@
+import pytest
+
+import cdx_toolkit
+
+
+def test_munge_filter():
+    tests = (('foo', 'foo', 'foo'),
+             ('!status:200', '!statuscode:200', '!status:200'),
+             ('statuscode:200', 'statuscode:200', 'status:200'),
+             ('url:foo', 'original:foo', 'url:foo'))
+
+    for t, ia, cc in tests:
+        assert cdx_toolkit.munge_filter(t, 'ia') == ia
+        assert cdx_toolkit.munge_filter(t, 'cc') == cc
+
+    with pytest.raises(ValueError):
+        assert cdx_toolkit.munge_filter('!=status:200', 'ia')
+
+
+def test_munge_fields():
+    tests = ((('statuscode', 'original'),
+              ([200, 'http://example.com/'],),
+              [{'status': 200, 'url': 'http://example.com/'}]),)
+
+    for fields, lines, ret in tests:
+        assert cdx_toolkit.munge_fields(fields, lines) == ret
+
--- tests/unit/test_misc.py
+++ tests/unit/test_misc.py
@@ -21,17 +21,3 @@ def test_showNumPages():
     with pytest.raises(ValueError):
         j_bad = MockResp('3')
         assert cdx_toolkit.showNumPages(j_bad) == 3
-
-
-def test_munge_filter():
-    tests = (('foo', 'foo', 'foo'),
-             ('!status:200', '!statuscode:200', '!status:200'),
-             ('statuscode:200', 'statuscode:200', 'status:200'),
-             ('url:foo', 'original:foo', 'url:foo'))
-
-    for t, ia, cc in tests:
-        assert cdx_toolkit.munge_filter(t, 'ia') == ia
-        assert cdx_toolkit.munge_filter(t, 'cc') == cc
-
-    with pytest.raises(ValueError):
-        assert cdx_toolkit.munge_filter('!=status:200', 'ia')
