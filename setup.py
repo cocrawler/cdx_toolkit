34ba7e1484d3f9cdfb7d7fa630d6145d63776f36
malteos <git@i.mieo.de>
2025-08-29 10:51:33 +0200

    End Python 3.7 support

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author_email='lindahl@pbm.com',
     url='https://github.com/cocrawler/cdx_toolkit',
     packages=packages,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     extras_require=extras_require,
     setup_requires=['setuptools-scm'],
     install_requires=requires,
@@ -60,7 +60,7 @@ setup(
         'Programming Language :: Python',
         #'Programming Language :: Python :: 3.5',  # setuptools-scm problem
         #'Programming Language :: Python :: 3.6',  # not offered in github actions
-        'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.7',  # end-of-life in 2023
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
