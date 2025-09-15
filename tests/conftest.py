972f69473e85836b8365e39459a8d56908771730
malteos <malte@commoncrawl.org>
2025-09-15 14:15:10 +0000

    adding save mock data via patch

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,13 @@ import json
 import os
 from pathlib import Path
 import functools
-from typing import Optional
+from typing import Dict, Optional
+import requests
 import responses
 import base64
+
+from unittest.mock import patch
+
 
 TEST_DATA_BASE_PATH = Path(__file__).parent / "data"
 
@@ -52,18 +56,13 @@ def flexible_param_matcher(expected_params):
 def mock_response_from_jsonl(mock_data_name, mock_data_dir: Optional[str] = None):
     """Load mock response data from JSONL file. Response match based on URL and request params.
 
-    To collect mock data, add this code snippet to the `myrequests.py` after the requests.get call:
-
-    ```python
-    import json
-    with open("./request_mock_data.jsonl", "a") as f:
-        f.write(json.dumps({
-            "method": "GET",
-            "url": url,
-            "request_params": params,
-            "response_status_code": resp.status_code,
-            "response_text": resp.text,
-        }) + "\n")
+    To collect mock data, set the enviornment variable when running the tests:
+    ```bash
+    # set environment variable
+    export SAVE_MOCK_RESPONSES=./tmp/mock_responses
+    
+    # run the test for what mock data should be saved to $SAVE_MOCK_RESPONSES
+    pytest tests/test_cli.py::test_basic
     ```
     Make sure to empty the cache before collecting mock data (~/.cache/cdx_toolkit/).
 
@@ -104,11 +103,82 @@ def conditional_mock_responses(func):
     """
 
     if os.environ.get("DISABLE_MOCK_RESPONSES"):
+        # If this flag is detected, all mocking is disabled
         return func
 
+    if os.environ.get("SAVE_MOCK_RESPONSES"):
+        # Mock data is saved by capturing output from requests.get
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with patch('requests.get', side_effect=_custom_behavior_with_original(requests.get)):
+                return func(*args, **kwargs)
+        return wrapper
+
+    # Add responses.activate
+    return add_mock_responses(func)
+
+
+def save_response_as_mock_data(test_info: str, request_url: str, request_params: Dict, resp, output_base_dir: str):
+    """Save request and response for mock data."""
+    # Format: "path/to/test_file.py::TestClass::test_method (setup)"
+    test_file = Path(test_info.split("::")[0]).stem
+    test_name = test_info.split("::")[-1].split()[0]
+
+    output_dir = os.path.join(output_base_dir, test_file)
+
+    # Make sure output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Append to JSONL file
+    with open(os.path.join(output_dir, f"{test_name}.jsonl"), "a") as f:
+        if request_url.endswith(".gz"):
+            # encode bytes as base64 string if file is gzipped
+            response_text = base64.b64encode(resp.content).decode('utf-8')
+        else: 
+            response_text = resp.text
+        
+        f.write(json.dumps({
+            "method": "GET",
+            "url": request_url, 
+            "request_params": request_params, 
+            "response_status_code": resp.status_code,
+            "response_text": response_text,
+        }) + "\n")
+
+
+def _custom_behavior_with_original(original_func):
+    def custom_behavior(*args, **kwargs):
+        # Call the original "requests.get"
+        response = original_func(*args, **kwargs)
+
+        if "url" in kwargs:
+            request_url = kwargs.pop("url")
+        else:
+            request_url = args[0]
+
+        if "params" in kwargs:
+            request_params = kwargs.pop("params")
+        else:
+            request_params = args[1]
+
+        # Make sure this is run as a unit test
+        if os.environ.get("PYTEST_CURRENT_TEST"):
+            save_response_as_mock_data(
+                test_info=os.environ["PYTEST_CURRENT_TEST"],  
+                request_url=request_url,
+                request_params=request_params,
+                resp=response,
+                output_base_dir=os.environ.get("SAVE_MOCK_RESPONSES"),
+            )
+
+        return response
+    return custom_behavior
+
+
+def add_mock_responses(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # Load mock data for index calls
+        # Load mock data for index calls (same for many test functions)
         mock_response_from_jsonl("cc_collinfo")
 
         # Auto-load mock data based on function name
