5840c4c31b6bd60ae8fd07854c8cc6e343714bf5
malteos <malte@commoncrawl.org>
2025-09-19 11:55:07 +0000

    fix s3 access in action

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,9 @@ def check_aws_s3_access():
     """Check if AWS S3 access is available."""
     try:
         s3_client = boto3.client('s3')
-        # Try to list buckets as a simple check
-        s3_client.list_buckets()
+
+        # Try list objects on test bucket
+        s3_client.list_objects_v2(Bucket=TEST_S3_BUCKET, MaxKeys=1)
         return True
     except (NoCredentialsError, ClientError):
         return False
