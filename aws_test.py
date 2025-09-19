ce3e741bdd2f73461641417e80a71be0b9822e4b
malteos <git@i.mieo.de>
2025-09-19 13:49:47 +0200

    adding missing file

--- aws_test.py
+++ aws_test.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 import boto3
-from botocore.exceptions import NoCredentialsError, ClientError
 
 TEST_DATA_PATH = Path(__file__).parent / "data"
 TEST_S3_BUCKET = os.environ.get("CDXT_TEST_S3_BUCKET", "commoncrawl-ci-temp")
@@ -10,16 +9,23 @@ TEST_S3_BUCKET = os.environ.get("CDXT_TEST_S3_BUCKET", "commoncrawl-ci-temp")
 
 def check_aws_s3_access():
     """Check if AWS S3 access is available."""
-    try:
-        s3_client = boto3.client('s3')
-        # Try to list buckets as a simple check
-        s3_client.list_buckets()
 
-        s3_client.list_objects_v2(Bucket=TEST_S3_BUCKET, MaxKeys=1)
+    session = boto3.Session()
+    credentials = session.get_credentials()
+    if credentials:
+        print(f'Access Key: {credentials.access_key[:10]}...')
+        print(f'Secret Key: {"SET" if credentials.secret_key else "NOT SET"}')
+        print(f'Token: {"SET" if credentials.token else "NOT SET"}')
+    else:
+        print('No credentials found by boto3')
+        
+    s3_client = boto3.client('s3')
+    # Try to list buckets as a simple check
+    s3_client.list_buckets()
 
-        return True
-    except (NoCredentialsError, ClientError):
-        return False
+    s3_client.list_objects_v2(Bucket=TEST_S3_BUCKET, MaxKeys=1)
+
+    return True
 
 
 assert check_aws_s3_access()
